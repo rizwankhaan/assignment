ed7396f034a8d5fa2a7962bb5f27d1cd385cd1de
Rizwan Khan <rizwan2fb@gmail.com>
2022-03-18 10:33:27 +0530

    First Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Test Magento2 Module Assignment
---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   Custom_Asignment
+ * @author    Apptrian
+ * @copyright Copyright (c) 2021
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License
+ */
+ 
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Rizwankhaan_Assignment',
+    __DIR__
+);
